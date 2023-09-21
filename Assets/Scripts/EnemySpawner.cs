ab9933112287f485f74261f050832bbcea1d121c
AlexBlok1083 <alexblok50@gmail.com>
2023-09-21 14:19:30 +0200

    EnemySpawner
    Begin of the enemy spawner. And added the background.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private int enemyCount;
+    [SerializeField] private GameObject Enemy;
+
+    [Header("Delay")]
+    [SerializeField] private float delayBtwSpawns;
+
+
+    private float _spawnTimer;
+    private int _enemiesSpawned;
+
+    private 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
