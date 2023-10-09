d05a399763a853c62ffda4c4f55fa188f31ec9b2
AlexBlok1083 <alexblok50@gmail.com>
2023-10-09 11:54:42 +0200

    Tower Detect Enemy near
    Tower script detect's the Enemies that are near and add it to the list of enemies in range.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private List<Enemy> _enemies = new List<Enemy>();
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        //Enemy
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy newEnemy = other.GetComponent<Enemy>();
+            _enemies.Add(newEnemy);
+        }
+        Debug.Log("somthing entered");
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (_enemies.Contains(enemy))
+            {
+                _enemies.Remove(enemy);
+            }
+        }
     }
 }
